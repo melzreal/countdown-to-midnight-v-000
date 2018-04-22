3ec9c33ee867f223ed00a35e6831446391ea0807
Melissa Real <melissa.real@hotmail.com>
2018-04-22 18:44:06 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+def countdown(ints)
+  while ints > 0
+    puts "#{ints} SECOND(S)!"
+    countdown_with_sleep(1)
+    ints-= 1 
+  end
+  
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+  sleep(seconds)
 end
